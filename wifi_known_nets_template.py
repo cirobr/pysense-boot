89cd65204bc6a43d3feea3a71e31271633a6d604
Ciro B Rosa <ciro.rosa@orbytai.com>
2020-08-11 16:40:46 -0300

    changed resets library

---
+++ wifi_known_nets_template.py
@@ -0,0 +1,10 @@
+# to work properly please rename file by removing "_template"
+def ssid_list():
+
+    known_nets = {
+        'Asus.CBR-m':        {'pwd':'place_ssidpassword_here'},
+        'Orbyt-AI':          {'pwd':'place_ssidpassword_here'},
+        'Orbyt Network 2.4': {'pwd':'place_ssidpassword_here'}
+    }
+
+    return known_nets
